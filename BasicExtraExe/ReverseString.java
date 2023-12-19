3cd5c0c5e09f878f8af332b81326f5fe0adb8ee7
Svetlyaza <153219501+Svetlyaza@users.noreply.github.com>
2023-12-19 18:34:33 +0200

    Add files via upload

---
+++ BasicExtraExe/ReverseString.java
@@ -0,0 +1,15 @@
+package BasicExtraExe;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        StringBuilder sb = new StringBuilder(input);
+        String reversed = sb.reverse().toString();
+
+        System.out.println(reversed);
+    }
+}
